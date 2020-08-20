1d0b5fa302af4ff2bbf01b54ab82c6eff90e6475
951237 <951237@gmail.com>
2020-08-20 23:35:12 +0900

    Doc : Update

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
-TELEGRAM_TOKEN = os.environ.get('TELEGRAM_TOKEN',"")
+TELEGRAM_TOKEN = os.environ.get('TELEGRAM_TOKEN', "")
 CHAT_ID = os.environ.get("CHAT_ID", "")
 
 if not TELEGRAM_TOKEN or not CHAT_ID:
