5edf7bb21d87092a5bdac7a10febd0b1bf0e3b2e
951237 <951237@gmail.com>
2020-09-23 01:12:03 +0900

    no message

--- app.py
+++ app.py
@@ -7,8 +7,9 @@ from noti import send
 def create_soup(p_url):
     # 유저에이전트 설정 - what is my user-agent 검색
     headers = {
-        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36"
-    }
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) "
+                      "Chrome/84.0.4147.125 Safari/537.36"
+        }
 
     # 링크 정보 받아오기
     res = requests.get(p_url, headers=headers)
@@ -37,12 +38,12 @@ def today_english():
         for txt in eng_texts:
             result.append(txt.get_text())
         # result.append()
-    
+
         # 한글 지문 출력
         result.append('한글 대화')
         for txt in kor_texts:
             result.append(txt.get_text())
-    
+
         return result
     except:
         error = ["오류 : 오늘의 영어"]
@@ -50,7 +51,7 @@ def today_english():
 
 
 def today_weather(p_region, p_url):
-    try :
+    try:
         result = []
         result.append(f"===== 오늘의 {p_region} 날씨 =====")
         soup = create_soup(p_url)
@@ -66,7 +67,6 @@ def today_weather(p_region, p_url):
             desc_rainfall = weather_area.find("dd", attrs={"class": "desc_rainfall"}).get_text()  # 강수확률
             newline = '\n'
             result.append(f'날씨 요약 : {summary}{newline}현재 온도 : {current_degree} /  체감온도 : {degree_feel} / 강수확률 : {desc_rainfall}')
-
 
             ttl_areas = div.find_all('div', attrs={"class": "ttl_area"})  # 세부날씨 정보
             charts = div.find_all('div', attrs={"class": "chart"})  # 세부날씨 수치
@@ -85,14 +85,15 @@ def today_weather(p_region, p_url):
 
             newline = '\n'
 
-            result.append(f'미세먼지 : {dust}({value}) / 초미세먼지 : {cho_dust}({cho_value}) / 자외선 : 'f'{sun}({sun_value}){newline}')
+            result.append(f'미세먼지 : {dust}({value}) / 초미세먼지 : {cho_dust}({cho_value}) / 자외선 : {sun}({sun_value}){newline}')
             return result
 
         except:
-            error = ["오류 : 오늘의 날씨 세부정보"]
-            return error
+            return ["오류 : 오늘의 날씨 세부정보"]
+
 
 # 열독률 높은 뉴스 - 예전 크롤링 기법
+
 def today_news():
     try:
         result = []
@@ -103,26 +104,25 @@ def today_news():
         box_headline = soup.find("div", {"class": "box_headline"})
         lis = box_headline.select('ul > li')
         i = 1
-    
+
         for li in lis:
             content = li.find_all('strong')
-            txt = content[0].text.strip().replace('\n'," / ")
+            txt = content[0].text.strip().replace('\n', " / ")
             link = li.find('a').get('href')
             result.append(f'{i}. {txt} / {link}')
             i += 1
         result.append("")
-    
+
         return result
 
     except:
-        result = ["오류 : 오늘의 뉴스"]
-        return result
+        return ["오류 : 오늘의 뉴스"]
 
 
 if __name__ == "__main__":
     # URL[0] : 새솔동, URL[1] : 안산시 사동
-    URL = {'새솔동' : 'https://n.weather.naver.com/today/02590140', '안산' : 'https://n.weather.naver.com/today/02271103'}
-    for k,v in URL.items():
+    URL = {'새솔동': 'https://n.weather.naver.com/today/02590140', '안산': 'https://n.weather.naver.com/today/02271103'}
+    for k, v in URL.items():
         weather_data = today_weather(k, v)
         weather = ("\n".join(str(i) for i in weather_data))
         send(weather)
